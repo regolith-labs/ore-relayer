54c103630d80098a05a971fc78e45e9dbd2075ae
alex <aabinaei@gmail.com>
2024-07-09 12:23:56 -0700

    new program-id

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -10,4 +10,4 @@ use solana_program::declare_id;
 
 // TODO
 // declare_id!("stakeHF5r6S7HyD9SppBfVMXMavDkJsxwGesEvxZr2A");
-declare_id!("25C1Mc8fuirWVR6PiNUFnQavLG7hetCr5iU3AY94pqbt");
+declare_id!("AUdvHZ5NkWrUHrGHuG4trGVTzCo4Dzg4XfbJaXf1WKwN");
